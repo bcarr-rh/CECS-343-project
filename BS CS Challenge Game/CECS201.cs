00a502db95475eeccdd0b757da535dd4667a9b1d
Adonias Lopez <csuadonias@yahoo.com>
2015-11-30 00:44:37 -0800

    Cards added for bonus points

---
+++ BS CS Challenge Game/CECS201.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS_CS_Challenge_Game
+{
+    class CECS201 : CardInterface
+    {
+        private string thisImage;
+        
+        public CECS201()
+        {
+            thisImage = "CECS201";
+        }
+        public override string getImage()
+        {
+            return thisImage;
+        }
+
+        public override string Play(Player p)
+        {
+            /*if (p.getCurrentRoom() == 14)
+            {
+                p.addLChip(1);
+                return (p.getPlayerName() + " played Professor Murgolo's CECS 174 Class for 1 learning chip");
+            }*/
+            return (p.getPlayerName() + " played " + thisImage + " FAILED");
+        }
+    }
+}
---
+++ BS CS Challenge Game/CECS228.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS_CS_Challenge_Game
+{
+    class CECS228 : CardInterface
+    {
+        private string thisImage;
+
+        public CECS228()
+        {
+            thisImage = "CECS228";
+        }
+
+        public override string getImage()
+        {
+            return thisImage;
+        }
+
+        public override string Play(Player p)
+        {
+            /*if (p.getCurrentRoom() == 14)
+            {
+                p.addLChip(1);
+                return (p.getPlayerName() + " played Professor Murgolo's CECS 174 Class for 1 learning chip");
+            }*/
+            return (p.getPlayerName() + " played " + thisImage + " FAILED");
+        }
+    }
+}
